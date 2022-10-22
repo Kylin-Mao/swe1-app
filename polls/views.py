c6e953ca9557e576087740b42251b06f4a541b86
Kaining Mao <chrissmao99@gmail.com>
2022-10-21 23:06:53 -0400

    check

--- polls/views.py
+++ polls/views.py
@@ -36,7 +36,7 @@ def vote(request, question_id):
             "polls/detail.html",
             {
                 "question": question,
-                "error_message": "You didn't select a choice.",
+                "error_message": "You didn't select a choice man.",
             },
         )
     else:
